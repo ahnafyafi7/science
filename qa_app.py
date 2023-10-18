dd0f4b18190ed68ac39ef49ad548dec98ec4c16b
ahnafyafi7 <muhammadahnafyafi7@gmail.com>
2023-10-19 01:46:06 +0700

    add href to form kepuasan user

--- qa_app.py
+++ qa_app.py
@@ -177,7 +177,7 @@ def main():
     f"""
     <div style="display: flex; align-items: center; margin-left: 0;">
         <h1 style="display: inline-block;">SCIENCE</h1>
-        <sup style="margin-left:5px;font-size:small; color: red;">microteaching</sup>
+        <sup style="margin-left:5px;font-size:small;"><a href="https://forms.gle/ZNXra72KSbgamLsA6" style="text-decoration:none; color: red;">microteaching</a></sup>
     </div>
     """,
     unsafe_allow_html=True,
